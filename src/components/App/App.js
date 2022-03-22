ffc88c1d3128a5049c5f32989b4102f6ae504a48
AleksandrRazhev <alexandrrazh@gmail.com>
2022-03-22 21:21:56 +0300

    useEffect fixed

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,27 +1,19 @@
 import React, { useState, useEffect } from 'react';
 
-import './App.scss';
+import './app.scss';
+
+const arrValute = ['usd', 'eur', 'gbp', 'cny', 'jpy', 'czk', 'sek', 'tjs'];
+
+const arrObjValute = arrValute.map(item => {
+  const obj = {};
+  obj.valute = item.toUpperCase();
+  return obj;
+});
 
 const App = (props) => {
-  const arrValute = ['usd', 'eur', 'gbp', 'cny', 'jpy', 'czk', 'sek', 'tjs'];
 
   const [number, setNumber] = useState(props.dollars);
-
-  const arrObjValute = arrValute.map(item => {
-    const obj = {};
-    obj.valute = item.toUpperCase();
-    const [rate, setRate] = useState(null);
-    obj.rate = rate;
-    obj.setRate = setRate;
-    const [name, setName] = useState(null);
-    obj.name = name;
-    obj.setName = setName;
-    const [nominal, setNominal] = useState(null);
-    obj.nominal = nominal;
-    obj.setNominal = setNominal;
-    return obj;
-  });
-
+  
   useEffect(() => {
     console.log('use effect')
 
@@ -31,9 +23,9 @@ const App = (props) => {
         arrObjValute.forEach(item => {
           Object.entries(data.Valute).forEach(i => {
             if (i[0] === item.valute) {
-              item.setRate(() => i[1].Value);
-              item.setName(() => i[1].Name);
-              item.setNominal(() => i[1].Nominal);
+              item.rate = i[1].Value;
+              item.name = i[1].Name;
+              item.nominal = i[1].Nominal;
             }
           });
         })
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App/App';
 
+import './style/style.scss';
+
 ReactDOM.render(
   <App dollars={(139.5866).toFixed(2)} />,
   document.getElementById('root')
